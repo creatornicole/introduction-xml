a22718d8c039750e2ed2bb5bd2479d0e95b77819
Nicole Gottschall <ngottschall@web.de>
2023-02-06 22:20:25 +0100

    added own SAX ErrorHandler

--- PR05/demoSimpleXmlSaxReader/ProgramMain.java
+++ PR05/demoSimpleXmlSaxReader/ProgramMain.java
@@ -7,13 +7,18 @@ public class ProgramMain {
 		System.out.println("Demo SAX-Reader");
 		
 		String path = "D:\\Stuff\\Studium\\HSMW\\IF21wS1-B\\3_datenrepraesentation\\praktika\\PR05\\";
-		String xmlFile = "biblio_schema.xml";
+		String xmlFile = "biblio_schema.xml";		
+		
+		//ErrorHandler mit invalidier XML-Datei testen
+		//String xmlFile = "biblio_schema_invalid.xml";
 		
 		//SAX-Reader instanziieren und initialisieren
 		SimpleSaxReader mySaxReader = new SimpleSaxReader(path + xmlFile);
 		
 		//Eigenen ContentHandler registrieren
 		mySaxReader.setContentHandler(new MyContentHandler());
+		//Eigenen ErrorHandler registrieren
+		mySaxReader.setErrorHandler(new MyErrorHandler());
 		
 		//Parsen starten und SAX-Events erzeugen
 		mySaxReader.run();
