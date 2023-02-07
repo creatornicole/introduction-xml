f1b9a7714bc9010b6cb79d0b0b7510865c890d8e
Nicole Gottschall <ngottschall@web.de>
2023-02-07 14:37:19 +0100

    added files for PR07

---
+++ PR07/beispiele/firmenadressen/run_xslt.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem So funktioniert das Aufrufen von Xalan-Java in einer Batch-Datei
+rem -classpath ..\..\xalan-j_2_7_2\xalan.jar: Verweis auf die jar-Datei
+rem org.apache.xalan.xslt.Process: Verweis auf die Java-Klasse
+rem -xsltc: Aufruf des XSLT-Compilers
+java -classpath ..\..\xalan-j_2_7_2\xalan.jar org.apache.xalan.xslt.Process -xsltc -IN FirmenAdressen.xml -XSL FirmenAdressen.xsl -OUT FirmenAdressen.html -html
+pause
